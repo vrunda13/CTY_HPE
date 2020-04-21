01a0ae83aaee6f08bbd239207b777adb3c98faf2
vrunda13 <43112048+vrunda13@users.noreply.github.com>
2020-04-22 01:30:56 +0530

    Create pdfminer.py

---
+++ pdfminer.py
@@ -0,0 +1,38 @@
+import io
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = io.StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,
+                                  password=password,
+                                  caching=caching,
+                                  check_extractable=True):
+        interpreter.process_page(page)
+
+
+
+    fp.close()
+    device.close()
+    text = retstr.getvalue()
+    retstr.close()
+    f= open("clear.txt","w+")
+    f.write(text)
+    f.close
+    
+    convert_pdf_to_txt("ClearPass.pdf")
